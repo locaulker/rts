ced5e0fd658045674f06115184f55345a0b08005
Lawrence Caulker <locaulker@gmail.com>
2023-08-30 01:23:46 -0400

    end types around props

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-// import App from "./App"
-import GuestList from "./state/GuestList"
+
+import UserSearch from "./state/UserSearch"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <div>
-    <GuestList />
+    <UserSearch />
   </div>
 )
